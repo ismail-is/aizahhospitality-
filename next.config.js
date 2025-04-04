925eca75325e78ff749877c9fb50216010db4a09
mscreationweb <mscreation361@gmail.com>
2025-04-04 16:15:21 +0530

    final commit

--- next.config.js
+++ next.config.js
@@ -1,12 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: "export",
-    images: {
-      unoptimized: true, // Fix for GitHub Pages
-    },
-    basePath: "/aizahhospitality-", // Change this to your GitHub repo name
-    assetPrefix: "/aizahhospitality-",
-  };
-  
-  module.exports = nextConfig;
-  
+const nextConfig = {}
+
+module.exports = nextConfig
